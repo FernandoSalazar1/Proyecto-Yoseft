48ccc100784f7a2ef8f98ced587bd9ff070ffcb4
FernandoSalazar1 <118940430+FernandoSalazar1@users.noreply.github.com>
2022-11-25 00:09:31 -0500

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { reducers, effects } from './store';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({}),
+    EffectsModule.forRoot(effects)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
